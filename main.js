2fd24217e687e3b78eec218340cdcde7fe1d0567
sleeptightwakebright@hotmail.com <sleeptightwakebright@hotmail.com>
2022-11-29 21:47:08 +1100

    good update, got two transitions working

---
+++ main.js
@@ -0,0 +1,25 @@
+let forest = document.getElementsByClassName('forest')[0];
+let sand = document.getElementsByClassName('sand')[0];
+let teal = document.getElementsByClassName('teal')[0];
+let navy = document.getElementsByClassName('navy')[0];
+let red = document.getElementsByClassName('red')[0];
+let babyBlue = document.getElementsByClassName('babyBlue')[0];
+
+let elements = [forest, sand, teal, navy, red, babyBlue]; 
+
+const transition = () => {
+    elements.forEach(e => {
+        console.log(e.id.includes('2'));
+        if (e.id.includes('1')) {
+            e.id = e.id.substring(0, e.id.length - 1) + '2';
+        } 
+        else {
+            e.id = e.id.substring(0, e.id.length - 1) + '1';
+        }
+    });
+}
+
+setInterval( 
+    transition,
+    4000
+);
