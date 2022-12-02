cf0de2df620eb06ad99d946571cba89a97bd4ce5
sleeptightwakebright@hotmail.com <sleeptightwakebright@hotmail.com>
2022-12-02 19:36:00 +1100

    started adding in mixed messages

---
+++ mixed-messages.js
@@ -0,0 +1,16 @@
+/* 
+Project Milestones
+- make my transitions project work
+- make my mix messages project work 
+- make my transitions accept message text nicely 
+- use modules to pull in mixed messages code 
+- add listeners write mixed message text into transition elements
+- optional: add a form element box and use what is writting into the form input
+
+Mixed Messages goals
+- make an array of nouns
+- make an array of strings
+- make a function that randomly selects three nouns and adds them into a random string, returning result
+*/
+
+
